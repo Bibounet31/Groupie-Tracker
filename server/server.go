76c9f7708bbf1a292bf4017a937e2bc0a3854840
bibounet <donot@gmail.com>
2025-12-11 16:54:55 +0100

    feat(server): tested server comm

--- server/server.go
+++ server/server.go
@@ -2,44 +2,8 @@ package server
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
-
-type PageData struct {
-	A int
-}
-
-var counter int // global variable
-
-func render(w http.ResponseWriter, file string, data any) {
-	funcMap := template.FuncMap{
-		"add": func(a, b int) int { return a + b },
-	}
-
-	t, err := template.New(file).Funcs(funcMap).ParseFiles("web/html/" + file)
-	if err != nil {
-		http.Error(w, "Template not found", 500)
-		return
-	}
-	t.Execute(w, data)
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	data := PageData{A: counter}
-	render(w, "index.html", data)
-}
-
-func submitHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Redirect(w, r, "/", 303)
-		return
-	}
-
-	counter++ // increase the value
-
-	http.Redirect(w, r, "/", 303)
-}
 
 func Start() {
 	http.HandleFunc("/", indexHandler)
